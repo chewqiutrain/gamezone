ed87f6742bf162320f879b7d94384448f75ec1f8
yingqiu <yingqiu@stashaway.com>
2021-05-23 20:05:03 +0800

    end of #33 showing form errors

--- screens/reviewForm.js
+++ screens/reviewForm.js
@@ -2,6 +2,27 @@ import React from 'react';
 import { StyleSheet, Button, TextInput, View, Text } from 'react-native';
 import { globalStyles } from '../styles/global.js';
 import { Formik } from 'formik'; 
+import * as yup from 'yup';
+
+// keys will be form fields
+// values are some validation 
+/**
+ * yup.string().required().min(4) 
+ *      data must be string, is required, must have minimum 4 chars 
+ */
+const reviewSchema = yup.object({
+    title: yup.string()
+        .required()
+        .min(4),
+    body: yup.string()
+        .required()
+        .min(8),
+    rating: yup.string() // by default data in inputForms are string. 
+        .test('is-num-1-5', 'Rating must be a number between 1-5', (val) => {
+            return parseInt(val) < 6 && parseInt(val) > 0;
+        })
+})
+
 //actions contains different methods we can use
 export default function ReviewForm({ addReview }) {
     return (
@@ -12,6 +33,7 @@ export default function ReviewForm({ addReview }) {
                     body: '',
                     rating: ''
                 }}
+                validationSchema={reviewSchema}
                 onSubmit={ (values, actions) => {
                     actions.resetForm();
                     console.log(`${JSON.stringify(values)}`);
@@ -25,14 +47,18 @@ export default function ReviewForm({ addReview }) {
                             placeholder='Review Title'
                             onChangeText={props.handleChange('title')}
                             value={props.values.title}
+                            onBlur={props.handleBlur('title')}
                         />
+                        <Text style={globalStyles.errorText}>{ props.touched.title && props.errors.title }</Text>
                         
                         <TextInput 
                             style={globalStyles.input}
                             placeholder='Review Body'
                             onChangeText={props.handleChange('body')}
                             value={props.values.body}
+                            onBlur={props.handleBlur('body')}
                         />
+                        <Text style={globalStyles.errorText}>{ props.touched.body && props.errors.body }</Text>
                         
                         <TextInput 
                             style={globalStyles.input}
@@ -40,7 +66,9 @@ export default function ReviewForm({ addReview }) {
                             onChangeText={props.handleChange('rating')}
                             value={props.values.rating}
                             keyboardType='numeric'
+                            onBlur={props.handleBlur('rating')}
                         />
+                        <Text style={globalStyles.errorText}>{ props.touched.rating && props.errors.rating }</Text>
 
                         <Button title="Submit" color="maroon" onPress={props.handleSubmit}/>
                     </View>
@@ -54,3 +82,5 @@ export default function ReviewForm({ addReview }) {
 //props.handleChange('title') updates the title property within the props object 
 
 //onPress={props.handleSubmit} runs the function defined at Formik onSubmit 
+
+//onBlur is triggered when the user leaves the field of entry 
