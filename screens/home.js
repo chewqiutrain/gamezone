b052e452120aa10f427db2e1e4c6fb866a67d46d
yingqiu <yingqiu@stashaway.com>
2021-05-23 18:39:01 +0800

    end of #29: modals

--- screens/home.js
+++ screens/home.js
@@ -1,15 +1,19 @@
 import React, { useState } from 'react';
-import { Button, FlatList, View, StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { Button, FlatList, View, StyleSheet, Modal, Text, TouchableOpacity } from 'react-native';
 import { globalStyles } from '../styles/global';
 import Card from '../shared/card';
+import { MaterialIcons } from '@expo/vector-icons';
 
 export default function Home({ navigation }) { // can take entire props object, or destructure this way
+
+    const [modalOpen, setModalOpen] = useState(false);
 
     const [reviews, setReviews] = useState([
         { title: "Zelda, Breath of Fresh Air", rating: 5, body: "Lorem ipsum", key: "1"},
         { title: "Gotta Catch Them All (again)", rating: 4, body: "Lorem ipsum", key: "2"},
         { title: "Not so 'Final' Fantasy", rating: 3, body: "Lorem ipsum", key: "3"},
     ])
+    
 
     const pressHandler = () => {
         console.log("In Home, button pressed, going to ReviewDetails screen")
@@ -20,10 +24,33 @@ export default function Home({ navigation }) { // can take entire props object, 
         navigation.push('reviewDetails') 
     }
 
+    //modal visible true will cause it to take the whole screen
+    // we want to allow a user to click a button to show the modal
     return (
         <View style={globalStyles.container}>
             {/* <Text style={globalStyles.titleText}>Home Screen</Text>
             <Button title='Go to review details' onPress={pressHandler}/> */}
+
+            <Modal visible={modalOpen} animationType='slide'> 
+                <View style={styles.modalContent}>
+                    <MaterialIcons 
+                    name='close'
+                    size={24}
+                    style={ { ...styles.modalToggle, ...styles.modalClose} }
+                    onPress={() => setModalOpen(false)}
+                    />
+                    <Text>Hello from the Modal!</Text>
+                </View>
+
+            </Modal>
+
+            <MaterialIcons 
+                name='add'
+                size={24}
+                style={styles.modalToggle}
+                onPress={() => setModalOpen(true)}
+            />
+
             <FlatList 
                 data={reviews}
                 renderItem={({ item }) => (
@@ -39,3 +66,21 @@ export default function Home({ navigation }) { // can take entire props object, 
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    modalContent: {
+        flex: 1
+    },
+    modalToggle: {
+        marginBottom: 10,
+        borderWidth: 1,
+        borderColor: "#f2f2f2",
+        padding: 10,
+        borderRadius: 1,
+        alignSelf: "center"      
+    },
+    modalClose: {
+        marginTop: 20,
+        marginBottom: 0
+    }
+})
