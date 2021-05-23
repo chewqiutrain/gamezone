c6a683f1f5f58341c79cf2d5d96c8c1e1d0a5b90
yingqiu <yingqiu@stashaway.com>
2021-05-23 20:15:04 +0800

    end of #34 custom button

--- screens/reviewForm.js
+++ screens/reviewForm.js
@@ -3,6 +3,7 @@ import { StyleSheet, Button, TextInput, View, Text } from 'react-native';
 import { globalStyles } from '../styles/global.js';
 import { Formik } from 'formik'; 
 import * as yup from 'yup';
+import FlatButton from '../shared/button';
 
 // keys will be form fields
 // values are some validation 
@@ -70,7 +71,8 @@ export default function ReviewForm({ addReview }) {
                         />
                         <Text style={globalStyles.errorText}>{ props.touched.rating && props.errors.rating }</Text>
 
-                        <Button title="Submit" color="maroon" onPress={props.handleSubmit}/>
+                        {/* <Button title="Submit" color="maroon" onPress={props.handleSubmit}/> */}
+                        <FlatButton text='submit' onPress={props.handleSubmit}/>
                     </View>
                 )}
             </Formik>
